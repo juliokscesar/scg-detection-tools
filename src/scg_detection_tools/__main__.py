f3d98722a05be4eb75e76a364949f5c271c66346
Julio Cesar <jcmcs177@gmail.com>
2024-09-02 15:58:16 -0300

    moved generate_dataset to a function in 'generate.py' NOT TESTED YET

--- src/scg_detection_tools/__main__.py
+++ src/scg_detection_tools/__main__.py
@@ -44,7 +44,7 @@ type=float,
     det_parser.add_argument("-s", 
                             "--slice-detect", 
                             action="store_true", 
-                            dest="slice",
+dest="slice",
                             help="Use slice detection")
     det_parser.add_argument("--slice-w", 
                             type=int, 
@@ -273,13 +273,10 @@ def segment(args):
             save_image(annotated, name=f"seg{os.path.basename(img)}", dir="out")
 
 def generate(args):
-    import scg_detection_tools.detect as det
-    import scg_detection_tools.segment as seg
-    from scg_detection_tools.dataset import Dataset
+    from scg_detection_tools.generate import generate_dataset
 
     img_files = get_all_files_from_paths(*args.img_source, skip_ext=[".txt", ".detections", ".json"])
     
-    gen_dataset = Dataset(name="gen_dataset", dataset_dir=args.out_dir, classes=args.data_classes)
 
     model_t = args.model_type
     if model_t == "yolov8":
@@ -289,94 +286,122 @@ def generate(args):
     else:
         raise RuntimeError(f"Support for model {model_t} not implemented for dataset generation")
 
-    detector = det.Detector(model, detection_params=None, specific_det_params={"use_slice": args.slice_detect})
-    for img in img_files:
-        curr_data = gen_dataset.get_data(mode="train")
-        if args.boxes:
-            from scg_detection_tools.utils.cvt import detbox_to_yolo_fmt
-            if args.on_slice:
-                def save_slice_callback(img_path, sliceimg, tmppath, det_boxes):
-                    slice_img_path = f".temp/slice_det{len(curr_data)}_{os.path.basename(img)}"
-                    shutil.copyfile(src=tmppath, dst=slice_img_path)
-
-                    fmt_boxes = detbox_to_yolo_fmt(boxes=det_boxes, imgsz=sliceimg.shape[1::-1]) # sliceimg.shape is (h,w,c) but we want (w,h)
-                    slice_ann = []
-                    for box in fmt_boxes:
-                        slice_ann.append([0])
-                        slice_ann[-1].extend(box)
-
-                    gen_dataset.add(img_path=slice_img_path, annotations=slice_ann)
-                detections = detector.detect_objects(img=img, use_slice=True, embed_slice_callback=save_slice_callback)[0]
-            else:
-                detections = detector.detect_objects(img)[0]
-                fmt_boxes = detbox_to_yolo_fmt(boxes=detections.xyxy, imgsz=cv2.imread(img).shape[1::-1])
-                img_ann = []
-                for box in fmt_boxes:
-                    img_ann.append([0])
-                    img_ann[-1].extend(box)
-
-                gen_dataset.add(img_path=img, annotations=img_ann)
-
-            print(f"GENERATOR: finished detections for image {img}. Counted: {len(detections.xyxy)}")
-
-        elif args.segments and args.cached_detections:
-            from scg_detection_tools.utils.cvt import contour_to_yolo_fmt
-            cache_loc = args.cached_detections
-            cache_files = get_all_files_from_paths(cache_loc, skip_ext=[".png", ".jpeg", ".jpg"])
-            boxes = None
+    imgboxes = None
+    if args.cached_detections:
+        cache_loc = args.cached_detections
+        cache_files = get_all_files_from_paths(cache_loc, skip_ext=[".png", ".jpeg", ".jpg"])
+        imgboxes = {}
+        for img in img_files:
             for cache_file in cache_files:
                 # if image is img.png, cache file will be img.png.detections
                 if Path(cache_file).stem == os.path.basename(img):
-                    boxes = read_detection_boxes_file(cache_file)
+                    imgboxes[img] = read_detection_boxes_file(cache_file)
                     break
-            if boxes is None:
-                warnings.warn(f"No cached detection file found for {img}, skipping...")
-                continue
-            sg = seg.SAM2Segment(sam2_ckpt_path=args.sam2_ckpt,
-                                 sam2_cfg=args.sam2_cfg,
-                                 detection_assist_model=model)
-            masks = sg._segment_boxes(img_p=img, boxes=boxes)
-            contours = sg._sam2masks_to_contours(masks)
-            fmt_contours = contour_to_yolo_fmt(contours, imgsz=(cv2.imread(img).shape[1::-1]))
-            img_ann = []
-            for contour in fmt_contours:
-                img_ann.append([0])
-                img_ann[-1].extend(contour)
-            gen_dataset.add(img_path=img, annotations=img_ann)
+    
+    gen_dataset = generate_dataset(name="gen_dataset",
+                                   out_dir="gen_out",
+                                   img_files=img_files,
+                                   classes=args.data_classes,
+                                   model=model,
+                                   sam2_ckpt_path=args.sam2_ckpt_path,
+                                   sam2_cfg=args.sam2_cfg,
+                                   use_boxes=args.boxes,
+                                   use_segments=args.segments,
+                                   gen_on_slice=args.on_slice,
+                                   slice_detect=args.slice_detect,
+                                   imgboxes_for_segments=imgboxes)
+
+    print("FINISHED GENERATING DATASET", gen_dataset._name)
 
 
-        elif args.segments:
-            from scg_detection_tools.utils.cvt import contour_to_yolo_fmt
-            sg = seg.SAM2Segment(sam2_ckpt_path=args.sam2_ckpt,
-                                 sam2_cfg=args.sam2_cfg,
-                                 detection_assist_model=model)
-            if args.on_slice:
-                seg_results = sg.slice_segment_detect(img_path=img, slice_wh=(640,640))
-                print(f"GENERATOR: finished detecitons and segmentations of slices for image {img}. Counted: {len(seg_results['detections'].xyxy)}")
-                
-                for slice in seg_results["slices"]:
-                    slice_tmp_path = slice["path"]
-                    slice_img_path = f".temp/slice_seg{len(curr_data)}_{os.path.basename(img)}"
-                    shutil.copyfile(src=slice_tmp_path, dst=slice_img_path)
 
-                    fmt_contours = contour_to_yolo_fmt(slice["contours"], imgsz=(640,640))
-                    slice_ann = []
-                    for contour in fmt_contours:
-                        slice_ann.append([0])
-                        slice_ann[-1].extend(contour)
-
-                    gen_dataset.add(img_path=slice_img_path, annotations=slice_ann)
-            else:
-                _, contours = sg.detect_segment(img, use_slice_detection=args.slice_detect)
-                fmt_contours = contour_to_yolo_fmt(contours, imgsz=cv2.imread(img).shape[1::-1])
-                img_ann = []
-                for contour in fmt_contours:
-                    img_ann.append([0])
-                    img_ann[-1].extend(contour)
-
-                gen_dataset.add(img_path=img, annotations=img_ann)
-
-        gen_dataset.save()
+    # for img in img_files:
+    #     curr_data = gen_dataset.get_data(mode="train")
+    #     if args.boxes:
+    #         from scg_detection_tools.utils.cvt import detbox_to_yolo_fmt
+    #         if args.on_slice:
+    #             def save_slice_callback(img_path, sliceimg, tmppath, det_boxes):
+    #                 slice_img_path = f".temp/slice_det{len(curr_data)}_{os.path.basename(img)}"
+    #                 shutil.copyfile(src=tmppath, dst=slice_img_path)
+    #
+    #                 fmt_boxes = detbox_to_yolo_fmt(boxes=det_boxes, imgsz=sliceimg.shape[1::-1]) # sliceimg.shape is (h,w,c) but we want (w,h)
+    #                 slice_ann = []
+    #                 for box in fmt_boxes:
+    #                     slice_ann.append([0])
+    #                     slice_ann[-1].extend(box)
+    #
+    #                 gen_dataset.add(img_path=slice_img_path, annotations=slice_ann)
+    #             detections = detector.detect_objects(img=img, use_slice=True, embed_slice_callback=save_slice_callback)[0]
+    #         else:
+    #             detections = detector.detect_objects(img)[0]
+    #             fmt_boxes = detbox_to_yolo_fmt(boxes=detections.xyxy, imgsz=cv2.imread(img).shape[1::-1])
+    #             img_ann = []
+    #             for box in fmt_boxes:
+    #                 img_ann.append([0])
+    #                 img_ann[-1].extend(box)
+    #
+    #             gen_dataset.add(img_path=img, annotations=img_ann)
+    #
+    #         print(f"GENERATOR: finished detections for image {img}. Counted: {len(detections.xyxy)}")
+    #
+    #     elif args.segments and args.cached_detections:
+    #         from scg_detection_tools.utils.cvt import contour_to_yolo_fmt
+    #         cache_loc = args.cached_detections
+    #         cache_files = get_all_files_from_paths(cache_loc, skip_ext=[".png", ".jpeg", ".jpg"])
+    #         boxes = None
+    #         for cache_file in cache_files:
+    #             # if image is img.png, cache file will be img.png.detections
+    #             if Path(cache_file).stem == os.path.basename(img):
+    #                 boxes = read_detection_boxes_file(cache_file)
+    #                 break
+    #         if boxes is None:
+    #             warnings.warn(f"No cached detection file found for {img}, skipping...")
+    #             continue
+    #         sg = seg.SAM2Segment(sam2_ckpt_path=args.sam2_ckpt,
+    #                              sam2_cfg=args.sam2_cfg,
+    #                              detection_assist_model=model)
+    #         masks = sg._segment_boxes(img_p=img, boxes=boxes)
+    #         contours = sg._sam2masks_to_contours(masks)
+    #         fmt_contours = contour_to_yolo_fmt(contours, imgsz=(cv2.imread(img).shape[1::-1]))
+    #         img_ann = []
+    #         for contour in fmt_contours:
+    #             img_ann.append([0])
+    #             img_ann[-1].extend(contour)
+    #         gen_dataset.add(img_path=img, annotations=img_ann)
+    #
+    #
+    #     elif args.segments:
+    #         from scg_detection_tools.utils.cvt import contour_to_yolo_fmt
+    #         sg = seg.SAM2Segment(sam2_ckpt_path=args.sam2_ckpt,
+    #                              sam2_cfg=args.sam2_cfg,
+    #                              detection_assist_model=model)
+    #         if args.on_slice:
+    #             seg_results = sg.slice_segment_detect(img_path=img, slice_wh=(640,640))
+    #             print(f"GENERATOR: finished detecitons and segmentations of slices for image {img}. Counted: {len(seg_results['detections'].xyxy)}")
+    #
+    #             for slice in seg_results["slices"]:
+    #                 slice_tmp_path = slice["path"]
+    #                 slice_img_path = f".temp/slice_seg{len(curr_data)}_{os.path.basename(img)}"
+    #                 shutil.copyfile(src=slice_tmp_path, dst=slice_img_path)
+    #
+    #                 fmt_contours = contour_to_yolo_fmt(slice["contours"], imgsz=(640,640))
+    #                 slice_ann = []
+    #                 for contour in fmt_contours:
+    #                     slice_ann.append([0])
+    #                     slice_ann[-1].extend(contour)
+    #
+    #                 gen_dataset.add(img_path=slice_img_path, annotations=slice_ann)
+    #         else:
+    #             _, contours = sg.detect_segment(img, use_slice_detection=args.slice_detect)
+    #             fmt_contours = contour_to_yolo_fmt(contours, imgsz=cv2.imread(img).shape[1::-1])
+    #             img_ann = []
+    #             for contour in fmt_contours:
+    #                 img_ann.append([0])
+    #                 img_ann[-1].extend(contour)
+    #
+    #             gen_dataset.add(img_path=img, annotations=img_ann)
+    #
+    #     gen_dataset.save()
 
 
 def main():
--- src/scg_detection_tools/generate.py
+++ src/scg_detection_tools/generate.py
@@ -1,2 +1,98 @@
+from typing import List
+import shutil
 
+from scg_detection_tools.models import BaseDetectionModel
+from scg_detection_tools.detect import Detector
+from scg_detection_tools.dataset import Dataset
+import scg_detection_tools.utils.cvt as cvt
+
+def generate_dataset(name: str, 
+                     out_dir: str,
+                     img_files: List[str],
+                     classes: List[str],
+                     model: BaseDetectionModel,
+                     sam2_ckpt_path: str = None,
+                     sam2_cfg: str = None,
+                     use_boxes=False,
+                     use_segments=False,
+                     gen_on_slice=False,
+                     slice_detect=False,
+                     imgboxes_for_segments: dict = None):
+    if (not use_boxes) or (not use_segments):
+        raise ValueError("generate_dataset require either use_boxes or use_segments to be true")
+
+    gen_dataset = Dataset(name=name, dataset_dir=out_dir, classes=classes)
+    
+    detector = Detector(model, specific_det_params={"use_slice": slice_detect})
+    for img in img_files:
+        # keep track of current amount of images to correctly name images
+        curr_data_len = len(gen_dataset.get_data(mode="train"))
+
+        ######################################################################################
+
+        if use_boxes:
+            if gen_on_slice:
+                def _save_slice_callback(img_path, slceimg, tmppath, det_boxes):
+                    slice_img_path = f".temp/slice_det{curr_data_len}_{os.path.basename(img_path)}"
+                    shutil.copyfile(src=tmppath, dst=slice_img_path)
+
+                    slice_ann = annotation_boxes(det_boxes, sliceimg.shape[1::-1])
+                    gen_dataset.add(img_path=slice_img_path, annotations=slice_ann)
+            else:
+                detections = detector.detect_objects(img)[0]
+                img_ann = annotation_boxes(boxes=detections.xyxy, imgsz=cv2.imread(img).shape[1::-1])
+                gen_dataset.add(img_path=img, annotations=img_ann)
+
+        ######################################################################################
+
+        if use_segments:
+            if sam2_ckpt_path is None or sam2_cfg is None:
+                raise ValueError("Must provide sam2_ckpt_path and sam2_cfg arguments for generating on segments")
+
+            from scg_detection_tools.segment import SAM2Segment
+
+            seg = SAM2Segment(sam2_ckpt_path=sam2_ckpt_path,
+                              sam2_cfg=sam2_cfg,
+                              detection_assist_model=model)
+            
+            if gen_on_slice:
+                seg_results = seg.slice_segment_detect(img_path=img, slice_wh=(640,640))
+                for slice in seg_results["slices"]:
+                    tmp_path = slice["path"]
+                    slice_path = f".temp/slice_seg{curr_data_len}_{os.path.basename(img)}"
+                    slice_ann = annotation_contours(slice["contours"], imgsz=(640,640))
+                    gen_dataset.add(img_path=slice_path, annotations=slice_ann)
+            else:
+                if imgboxes_for_segments is not None:
+                    masks = seg._segment_boxes(img_p=img, boxes=imgboxes_for_segments[img])
+                    contours = seg._sam2masks_to_contours(masks)
+                else:
+                    _, contours = seg.detect_segment(img, use_slice_detection=slice_detect)
+
+                img_ann = annotation_contours(contours, imgsz=cv2.imread(img).shape[1::-1])
+                gen_dataset.add(img_path=img, annotations=img_ann)
+    
+        ######################################################################################
+        
+        # save after processing each image
+        gen_dataset.save()
+    
+    return gen_dataset
+            
+
+def annotation_boxes(det_boxes, imgsz):
+    fmt_boxes = cvt.detbox_to_yolo_fmt(det_boxes, imgsz)
+    ann = []
+    for box in fmt_boxes:
+        ann.append([0])
+        ann[-1].extend(box)
+    return ann
+
+def annotation_contours(contours, imgsz):
+    fmt_cnt = cvt.contour_to_yolo_fmt(contours, imgsz)
+    ann = []
+    for contour in fmt_cnt:
+        ann.append([0])
+        ann[-1].extend(contour)
+    return ann
 
